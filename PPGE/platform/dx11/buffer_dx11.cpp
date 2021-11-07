0ade32c6208996a17cd660f3bc07ba39e4923bea
Oguzhan Karakaya <moguzhankarakaya@gmail.com>
2021-11-07 15:06:31 +0100

    Start implementing D3D11 Buffer.

--- PPGE/platform/dx11/buffer_dx11.cpp
+++ PPGE/platform/dx11/buffer_dx11.cpp
@@ -2,4 +2,8 @@
 
 namespace PPGE
 {
+BufferD3D11::BufferD3D11(RendererSystemDX11 &renderer_ref, const BufferDesc &desc) : m_renderer_ref(renderer_ref)
+{
+
 }
+} // namespace PPGE
--- PPGE/platform/dx11/buffer_dx11.h
+++ PPGE/platform/dx11/buffer_dx11.h
@@ -3,12 +3,23 @@
 
 #include "core/defines.h"
 #include "system/logger_system.h"
-#include "platform/dx11/renderer_system_dx11.h"
+#include "renderer/buffer.h"
 
 namespace PPGE
 {
+class RendererSystemDX11;
+
 class PPGE_API BufferD3D11
 {
+    BufferD3D11(RendererSystemDX11 &renderer_ref, const BufferDesc &desc);
+
+    virtual void CreateBuffer();
+    virtual void UpdateBuffer();
+    virtual void SetBuffer();
+
+  protected:
+    RendererSystemDX11 &m_renderer_ref;
+    ID3D11Buffer *m_buffer;
 };
 
 class PPGE_API IndexBufferD3D11 : public BufferD3D11
@@ -17,5 +28,8 @@ class PPGE_API IndexBufferD3D11 : public BufferD3D11
 
 class PPGE_API VertexBufferD3D11 : public BufferD3D11
 {
+    VertexBufferD3D11(RendererSystemDX11 &renderer, const VertexBufferDesc &desc);
+
+    protected:
 };
-}
+} // namespace PPGE

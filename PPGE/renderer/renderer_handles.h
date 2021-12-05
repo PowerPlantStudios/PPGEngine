8616cb38d2d8112f470343561d83a449a8cffa56
Oguzhan Karakaya <moguzhankarakaya@gmail.com>
2021-12-05 16:07:18 +0100

    [WIP] [DX11] [PPGE::Renderer] Renderer Architecture

--- PPGE/renderer/renderer_handles.h
+++ PPGE/renderer/renderer_handles.h
@@ -11,15 +11,28 @@ PPGE_HANDLE(IndexBufferHandle, PPGE_RENDERER_MAX_INDEX_BUFFERS);
 PPGE_HANDLE(ProgramHandle, PPGE_RENDERER_MAX_PROGRAMS);
 PPGE_HANDLE(UniformHandle, PPGE_RENDERER_MAX_UNIFORMS);
 
-template <typename HandleType> class HandleAllocator
+template <typename HandleType, typename Idx_t = uint16_t> class HandleAllocator
 {
     HandleAllocator() : m_available_handles(HandleType::GetMaxIdx())
     {
     }
 
+    HandleType GetNext()
+    {
+        Idx_t idx = m_available_handles.back();
+        m_available_handles.pop_back();
+        HandleType handle{idx};
+        return handle;
+    }
+
+    void Restore(HandleType handle)
+    {
+        Idx_t idx = handle.idx;
+        m_available_handles.push_back(idx);
+    }
+
   private:
-    typedef uint16_t Idx_t;
-    std::vector<Idx> m_available_handles;
+    std::vector<Idx_t> m_available_handles;
 };
 
 } // namespace PPGE
